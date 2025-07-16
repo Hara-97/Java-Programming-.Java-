a2eddad343f6f36188e7a76ae3ec050ea4830803
Hara-97 <240301120382@centurionuniv.edu.in>
2025-07-16 18:47:24 +0530

    Update age.java

--- age.java
+++ age.java
@@ -1,8 +1,8 @@
-
+// 2.	Write a program to declare an integer variable age and initialize it with your current age. Then, print its value.
 
 public class age {
     public static void main(String[] args) {
-        int age = 25; // Replace 25 with your current age if needed
+        int age = 25;
         System.out.println("Age: " + age);
     }
 }
