5bf3b5e9bc644bf8ca7d01559260626fe1b48f38
Hara-97 <240301120382@centurionuniv.edu.in>
2025-07-16 18:46:38 +0530

    Add files via upload

---
+++ age.java
@@ -0,0 +1,8 @@
+
+
+public class age {
+    public static void main(String[] args) {
+        int age = 25; // Replace 25 with your current age if needed
+        System.out.println("Age: " + age);
+    }
+}
