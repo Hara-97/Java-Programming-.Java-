2c16fc98436f9fab02f3bd3fffb4651b76c99e8b
Hara-97 <240301120382@centurionuniv.edu.in>
2025-07-18 20:26:55 +0530

    Update SumTwoNumbers.java

--- SumTwoNumbers.java
+++ SumTwoNumbers.java
@@ -5,17 +5,14 @@ public class SumTwoNumbers {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // Take two integer inputs
+        
         System.out.print("Enter first number: ");
         int num1 = sc.nextInt();
 
         System.out.print("Enter second number: ");
         int num2 = sc.nextInt();
 
-        // Calculate sum
         int sum = num1 + num2;
-
-        // Print result
         System.out.println("Sum = " + sum);
 
         sc.close();
